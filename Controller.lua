3d1ede1a9a28fc3655f65834f18104f838f8ddd9
Magerles <s.magerle@gmx.de>
2025-01-05 13:53:41 +0100

    v2

--- Controller.lua
+++ Controller.lua
@@ -5,6 +5,12 @@ local size = {
     y=0
 }
 
+local graphic_window = {
+    xmin=0,
+    xmax=0,
+    ymin=0,
+    ymax=0
+}
 
 
 --Reactor Variables
@@ -26,8 +32,43 @@ local function initMonitor()
     monitor.setTextColor(colors.green)
     monitor.setBackgroundColor(colors.black)
     size.x,size.y= monitor.getSize()
+    graphic_window.xmin=(1)
+    graphic_window.xmax=(2/3*size.x)
+    graphic_window.ymin=(1)
+    graphic_window.ymax=(size.y-10)
     monitor.setCursorPos(1,1)
     return true
+end
+
+local function drawRectangle(x1, y1, x2, y2, infill, color)
+    if x1 > x2 then x1, x2 = x2, x1 end
+    if y1 > y2 then y1, y2 = y2, y1 end
+
+   
+    monitor.setTextColor(colors[color] or colors.white)
+
+    if not infill then
+        monitor.setCursorPos(x1, y1)
+        monitor.write(string.rep(" ", x2 - x1 + 1))
+
+        monitor.setCursorPos(x1, y2)
+        monitor.write(string.rep(" ", x2 - x1 + 1))
+
+        for y = y1 + 1, y2 - 1 do
+            monitor.setCursorPos(x1, y)
+            monitor.write(" ")
+
+            monitor.setCursorPos(x2, y)
+            monitor.write(" ")
+        end
+    else
+        for y = y1, y2 do
+            monitor.setCursorPos(x1, y)
+            monitor.write(string.rep(" ", x2 - x1 + 1))
+        end
+    end
+
+    monitor.setTextColor(colors.white)
 end
 
 local function startUpScreen()
@@ -49,49 +90,61 @@ end
 local function postStatusUpdate()
     monitor.clear()
     monitor.setTextColor(colors.white)
-    monitor.setCursorPos(1,1)
-    monitor.write("Reactor Controller; Version 0.1")
-    monitor.setCursorPos(2,2)
+    monitor.setCursorPos(1,size.y)
+    monitor.write("Reactor Controller; Version 0.2")
+    monitor.setCursorPos(2,size.y-1)
     local filledString = string.rep("=", size.x-2)
     monitor.write(filledString)
 
-    monitor.setCursorPos(1,4)
+    monitor.setCursorPos(1,size.y-4)
     monitor.write("Status: ")
     if (Active) then
         monitor.setTextColor(colors.green)
+        monitor.write("active")
     end
     if (not Active) then
         monitor.setTextColor(colors.red)
+        monitor.write("disabled")
     end
-    monitor.write(Active)
+    
     monitor.setTextColor(colors.white)
 
-    --monitor.setCursorPos(1,5)
+    --monitor.setCursorPos(1,size.y-5)
     --monitor.write("Reactor Capacity: ")
     --monitor.write(Capacity)
 
-    --monitor.setCursorPos(1,6)
+    --monitor.setCursorPos(1,size.y-6)
     --monitor.write("Reactor Storage Volume: ")
     --monitor.write(Storage)
 
-    monitor.setCursorPos(1,7)
+    monitor.setCursorPos(1,size.y-7)
     monitor.write("Percentage of used Capacity: ")
     monitor.write(string.format("%.2f", StoragePercent))
 
-    monitor.setCursorPos(1,8)
+    monitor.setCursorPos(1,size.y-8)
     monitor.write("ControlRodsLevel: ")
     monitor.write(ControlRodsLevel[1])
 
-    monitor.setCursorPos(2,9)
-    local filledString = string.rep("=", size.x-2)
+    monitor.setCursorPos(2,size.y-9)
     monitor.write(filledString)
 
 end
 
 
-local function ControlReactor()
+local function generateGraphs()
+    local BufferString = "Energy Storage"
+    monitor.setCursorPos((graphic_window.xmax/2)-((#BufferString))/2,1)
+    monitor.write(BufferString)
+
+    drawRectangle(2,2,graphic_window.xmax-1,8,false,blue)
+
+
+
+end
+
+local function controlReactor()
     Capacity = reactor.getEnergyCapacity()
-    
+    generateGraphs()
     while (true) do
         Active = reactor.getActive()
         Storage= reactor.getEnergyStored()
@@ -106,8 +159,8 @@ local function ControlReactor()
             reactor.setActive(true)
         end 
         postStatusUpdate()
-    sleep(1)
-    end
+    sleep(0.1)
+    end 
 end
 
 local function main()
@@ -119,7 +172,7 @@ local function main()
     end
     print("Reactor found, this is where the fun begins!")
     sleep(3)    
-    ControlReactor()
+    controlReactor()
 
 
 
