36c635d7100f369ab223fd7b446451b80d2a820e
Magerles <s.magerle@gmx.de>
2025-01-05 03:15:36 +0100

    updated

--- Controller.lua
+++ Controller.lua
@@ -1,23 +1,34 @@
 local reactor
 local monitor = peripheral.find("monitor")
-local size{
+local size = {
     x=0,
     y=0
 }
 
+
+
+--Reactor Variables
+local Active=false
+local Capacity=0
+local Storage=0
+local ControlRodsLevel=0
+
+
 local StartUpMessage = {
     "Das ist Saschas ReaktorController!",
-    "Huldigt dem großen Rechner!"
+    "",
+    "Huldigt dem BigReactor!"
 }
 
 local function startUpScreen()
-    initMonitor()
+    
     local y=size.y/2
     for _, str in pairs(StartUpMessage) do
         monitor.setCursorPos((size.x/2)-((#str))/2,y)
         monitor.write(str)
         y=y+1
     end
+end
 
 local function initMonitor()
     monitor.clear()
@@ -26,6 +37,7 @@ local function initMonitor()
     monitor.setBackgroundColor(colors.black)
     size.x,size.y= monitor.getSize()
     monitor.setCursorPos(1,1)
+    return true
 end
 
 local function findReactor()
@@ -33,9 +45,72 @@ local function findReactor()
     return true 
 end
 
+local function postStatusUpdate()
+    monitor.clear()
+    monitor.setTextColor(colors.white)
+    monitor.setCursorPos(1,1)
+    monitor.write("Reaktor Controller; Version 0.1")
+    monitor.setCursorPos(2,2)
+    local filledString = string.rep("=", size.x-2)
+    monitor.write(filledString)
 
+    monitor.setCursorPos(1,4)
+    monitor.write("Reactor Status: ")
+    if (Active) then
+        monitor.setTextColor(colors.green)
+    end
+    if (not Active) then
+        monitor.setTextColor(colors.red)
+    end
+    monitor.write(Active)
+    monitor.setTextColor(colors.white)
+
+    monitor.setCursorPos(1,5)
+    monitor.write("Reactor Capacity: ")
+    monitor.write(Capacity)
+
+    monitor.setCursorPos(1,6)
+    monitor.write("Reactor Storage Volume: ")
+    monitor.write(Storage)
+
+    monitor.setCursorPos(1,7)
+    monitor.write("Percentage of used Capacity: ")
+    monitor.write(StoragePercent)
+
+    monitor.setCursorPos(1,8)
+    monitor.write("Percentage of used Capacity: ")
+    monitor.write(ControlRodsLevel)
+
+    monitor.setCursorPos(2,9)
+    local filledString = string.rep("=", size.x-2)
+    monitor.write(filledString)
+
+end
+
+
+local function ControlReactor
+    Capacity = reactor.getEnergyCapacity()
+    
+    while (true) do
+        Active = reactor.getActive()
+        Storage= reactor.getEnergyStored()
+        StoragePercent = Storage/Capacity
+        ControlRodsLevel=reactor.getControlRodsLevels()
+        
+        if (StoragePercent > 0.8) then
+            reactor.setActive(false)
+        end
+    
+        if (StoragePercent < 0.3) then
+            reactor.setActive(true)
+        end 
+        postStatusUpdate()
+    sleep(1)
+    end
+end
 
 local function main()
+    initMonitor()
     startUpScreen()
     while (not findReactor()) do 
         print("Reactor not found. Well... damn. I'll try again")
